7fcf523fc94044649d8e9ad663edc70c3dc747a5
he <123@email.com>
2025-10-21 20:13:44 +0800

    新增员工代码开发

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -54,7 +54,7 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
         } catch (Exception ex) {
             //4、不通过，响应401状态码
             response.setStatus(401);
-            return false;
+            return false
         }
     }
 }
